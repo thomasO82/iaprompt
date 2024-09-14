4f1eb5899618da2319fb138c6faeb5e0c0378d3c
o-thomas <thomas.oliver@ri7.fr>
2024-09-14 17:38:59 +0200

    dsdsd

--- server.js
+++ server.js
@@ -1,3 +1,20 @@
+const express = require('express');
+const cors = require('cors');
+require('dotenv').config();
+const OpenAI = require('openai');
+const app = express();
+
+app.use(cors());
+app.use(express.json());
+
+const openai = new OpenAI({
+    apiKey: process.env.OPENAI_API_KEY,
+});
+
+
+app.listen(3012, () => console.log('Server started on port 8080'));
+
+
 app.post('/api', async (req, res) => {
     try {
         const { prompt } = req.body;
@@ -16,3 +33,4 @@ app.post('/api', async (req, res) => {
         res.status(500).json({ error: "Une erreur est survenue lors de la génération de l'image" });
     }
 });
+
